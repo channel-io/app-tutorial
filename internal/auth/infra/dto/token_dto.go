cce3427aeccab61ae2230d214bafd9c24b0afe8e
vita <vita@channel.io>
2024-05-22 14:12:56 +0900

    fix: remove expires_in; unused

--- internal/auth/infra/dto/token_dto.go
+++ internal/auth/infra/dto/token_dto.go
@@ -12,5 +12,4 @@ type RefreshTokenParams struct {
 type TokenResponse struct {
 	AccessToken  string `json:"accessToken"`
 	RefreshToken string `json:"refreshToken"`
-	ExpiresIn    int64  `json:"expiresIn"`
 }
