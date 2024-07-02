1b999c1f8eb46f7c5300dda5bebe7744415478e0
choijiho <choijiho0021@gmail.com>
2024-07-02 14:56:11 +0900

    변경된 인터페이스 반영

--- wam/src/vite-env.d.ts
+++ wam/src/vite-env.d.ts
@@ -1,40 +1,42 @@
 /// <reference types="vite/client" />
 
 interface Window {
-  getWamData: (key: string) => string | null
-  close: ({
-    appId,
-    name,
-    params = null,
-  }: {
-    appId: string
-    name: string
-    params: Record<string, any>
-  } = {}) => void
-  setSize: ({ width, height }: { width: number; height: number }) => void
-  callFunction: ({
-    appId,
-    name,
-    params,
-  }: {
-    appId: string
-    name: string
-    params: Record<string, any>
-  }) => Promise<any>
-  callNativeFunction: ({
-    name,
-    params,
-  }: {
-    name: string
-    params: Record<string, any>
-  }) => Promise<any>
-  callCommand: ({
-    appId,
-    name,
-    params,
-  }: {
-    appId: string
-    name: string
-    params: Record<string, any>
-  }) => void
+  ChannelIOWam: {
+    getWamData: (key: string) => string | null
+    close: ({
+      appId,
+      name,
+      params = null,
+    }: {
+      appId: string
+      name: string
+      params: Record<string, any>
+    } = {}) => void
+    setSize: ({ width, height }: { width: number; height: number }) => void
+    callFunction: ({
+      appId,
+      name,
+      params,
+    }: {
+      appId: string
+      name: string
+      params: Record<string, any>
+    }) => Promise<any>
+    callNativeFunction: ({
+      name,
+      params,
+    }: {
+      name: string
+      params: Record<string, any>
+    }) => Promise<any>
+    callCommand: ({
+      appId,
+      name,
+      params,
+    }: {
+      appId: string
+      name: string
+      params: Record<string, any>
+    }) => void
+  }
 }
